cede1f9912ceb1b743aaeffd7b4b30505e2b802c
kingBook <412126604@qq.com>
2022-07-13 17:46:45 +0800

    create

---
+++ Assets/PanelTest.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PanelTest : MonoBehaviour {
+
+    [SerializeField] private Text m_text;
+
+    private ScreenOrientationController m_screenOrientationListener;
+
+    #region Editor References
+    public void OnClickButtonLandscape() {
+        Screen.orientation = ScreenOrientation.Landscape;
+    }
+
+    public void OnClickButtonPortrait() {
+        Screen.orientation = ScreenOrientation.Portrait;
+    }
+
+    public void OnClickButtonClear() {
+        m_text.text = "";
+    }
+    #endregion
+
+    private void Log(string str) {
+        Debug.Log(str);
+        m_text.text += $"{str}\n";
+    }
+
+    private void OnScreenRotatedHandler(ScreenOrientation orientation) {
+        //Log($"onRotated Screen.orientation:{Screen.orientation}, Screen.width:{Screen.width}, Screen.height:{Screen.height}");
+    }
+
+    private void Start() {
+        m_text.text = "";
+        m_screenOrientationListener = GetComponent<ScreenOrientationController>();
+        m_screenOrientationListener.onScreenRotatedEvent += OnScreenRotatedHandler;
+    }
+
+    private void Update() {
+        Log($"Screen.orientation:{Screen.orientation}, Screen.width:{Screen.width}, Screen.height:{Screen.height}");
+    }
+
+
+}
---
+++ Assets/ScreenOrientationController.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// 屏幕旋转侦听器
+/// <para> 注意: </para>
+/// <para> 1. 在进入第一帧之后开始侦听 </para>
+/// </summary>
+public class ScreenOrientationController : MonoBehaviour {
+
+    /// <summary>
+    /// 屏幕旋转完成事件，回调函数格式: <code> void OnScreenRotatedHandler(ScreenOrientation orientation) </code>
+    /// </summary>
+    public event System.Action<ScreenOrientation> onScreenRotatedEvent;
+
+    private ScreenOrientation m_oldScreenOrientation;
+    private int m_oldScreenWidth;
+    private bool m_onRotatedResumeAutoRotation;
+
+    public void SetOrientation(ScreenOrientation orientation, bool rotatedResumeAutoRotation) {
+        Screen.orientation = orientation;
+        m_onRotatedResumeAutoRotation = rotatedResumeAutoRotation;
+    }
+
+    private void Awake() {
+        StartCoroutine(InitCoroutine());
+    }
+
+    private IEnumerator InitCoroutine() {
+        yield return null;
+        m_oldScreenOrientation = Screen.orientation;
+        m_oldScreenWidth = Screen.width;
+    }
+
+    private void Update() {
+        if (m_oldScreenOrientation == 0) return;
+        if (m_oldScreenOrientation == Screen.orientation) return;
+        if (m_oldScreenWidth == Screen.width) return;
+
+        // 记录上一次的屏幕朝向与屏幕宽
+        m_oldScreenOrientation = Screen.orientation;
+        m_oldScreenWidth = Screen.width;
+
+        // 屏幕旋转完成后恢复自动旋转
+        if (m_onRotatedResumeAutoRotation) {
+            m_onRotatedResumeAutoRotation = false;
+            Screen.orientation = ScreenOrientation.AutoRotation;
+        }
+
+        // 派发完成事件
+        onScreenRotatedEvent?.Invoke(Screen.orientation);
+
+        
+    }
+
+}
